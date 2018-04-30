9681cb9495a067ac1d558df1d364fce8a4274848
namotch <namotch@gmail.com>
2018-04-30 23:26:11 +0900

    3.6.7 でエラーになるのを回避した（やっつけ）

--- world_primary.rb
+++ world_primary.rb
@@ -4,10 +4,14 @@ Plugin.create :world_primary do
 
   # 起動時に登録したアカウントに変更する
   onboot do |service|
-    world = Enumerator.new{|y| Plugin.filtering(:worlds, y) }.find{ |w|
-      w.slug.to_sym == UserConfig[:world_primary_account].to_s.to_sym
+    # 3.6.7 ではアカウント取得出来ないため、１秒だけ遅らせた
+    # アカウントがロードされてから処理を実行する何かいい方法があれば教えて下さい
+    Reserver.new(1) {
+      world = Enumerator.new{|y| Plugin.filtering(:worlds, y) }.find{ |w|
+        w.slug.to_sym == UserConfig[:world_primary_account].to_s.to_sym
+      }
+      Plugin.call(:world_change_current, world)
     }
-    Plugin.call(:world_change_current, world)
   end
 
   # 設定画面
